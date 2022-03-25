b39ef7c84e3eef419565ed29422ef21071915262
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2022-03-25 16:24:58 +0200

    Creating Project enviroment

---
+++ src/SearchEngine/Indexer.java
@@ -0,0 +1,10 @@
+package SearchEngine;
+
+public class Indexer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from indexer");
+	}
+
+}
