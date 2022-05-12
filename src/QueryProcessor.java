7ae53346d1d1b22cf47479e4bf5b629790d306bb
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-05-12 22:14:25 +0200

    PhraseSearchingIsDone
    yala y3m ay 5edmaa

--- src/QueryProcessor.java
+++ src/QueryProcessor.java
@@ -14,14 +14,17 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
 public class QueryProcessor {
-
 	public static int number_documents;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map <String , ArrayList<Integer>> mp = new HashMap<>() ; 
-		mp.put("Ahmed" , new ArrayList<Integer>());
-		mp.get("Ahmed").add(1); 
-		System.out.print(mp);
+
+		HashMap<String,ArrayList<Document>> map1 = Query_Process("even to the King and Queen"); 
+		ArrayList<String> URLS = phraseSearch(map1); 
+		int size = URLS.size() ; 
+		for(int i = 0 ; i < size ; i++)
+		{
+			System.out.println(URLS.get(i));
+		}
 		return ;
 	}
 	public static HashMap<String,ArrayList<Document>> Query_Process (String query)
@@ -35,7 +38,6 @@ public class QueryProcessor {
 		MongoCollection<Document> indexercol = Indexer.get_collection(indexerdb, "invertedfile");
 		
 		// connection with local
-		
 		//MongoClient mongoClient2 = MongoClients.create("mongodb://localhost:27017");
 		//MongoDatabase indexerdb = mongoClient2.getDatabase("Search_index");
 		//MongoCollection<Document> indexercol = Indexer.get_collection(indexerdb, "invertedfile");
@@ -83,7 +85,7 @@ public class QueryProcessor {
 	public static ArrayList<String> phraseSearch(HashMap<String,ArrayList<Document>>myMap){
 		
 		// 34an ana msh 3auzo ye3dl fl trteb
-		HashMap<String , ArrayList<Document>> UrlMap =  new LinkedHashMap<String,ArrayList<Document>>() ;
+		HashMap<String , ArrayList<Document>> UrlMap =  new LinkedHashMap<String,ArrayList<Document>>() ; // -> 
 		
 		Iterator<Entry<String , ArrayList<Document>>> myIterator = myMap.entrySet().iterator() ;
 		
@@ -94,16 +96,17 @@ public class QueryProcessor {
 			Map.Entry<String, ArrayList<Document>> newMap = myIterator.next(); 
 			
 			//iterating over its whole documents and storing it in the new hashmap
-			for(int i = 0 ; i < newMap.getValue().size(); i ++)
+			int size = newMap.getValue().size() ; 
+			for(int i = 0 ; i < size ; i ++)
 			{
 				// check if the url exists before in our map 
 				var check = UrlMap.get(newMap.getValue().get(i).getString("DOC_ID")); 
 				if(check != null) // if yes add this document to it
-					UrlMap.get(newMap.getValue().get(i).getString("DOC_ID")).add(newMap.getValue().get(i)); 
+					UrlMap.get(newMap.getValue().get(i).getString("DOC_ID")).add(newMap.getValue().get(i)); // el document el feha el esm -> ahmed masln
 				else // create new place to it and add the document in the List 
 				{
-					UrlMap.put(newMap.getValue().get(i).getString("DOC_ID"),new ArrayList<Document>()); 
-					UrlMap.get(newMap.getValue().get(i).getString("DOC_ID")).add(newMap.getValue().get(i)); 
+					UrlMap.put(newMap.getValue().get(i).getString("DOC_ID"),new ArrayList<Document>()); // create new slot with this URL as a key
+					UrlMap.get(newMap.getValue().get(i).getString("DOC_ID")).add(newMap.getValue().get(i)); // add the document in its array
 				}
 			}			
 		}
@@ -123,28 +126,28 @@ public class QueryProcessor {
 		{
 			Map.Entry<String, ArrayList<Document>> newMap = myIterator.next(); 
 			int size = newMap.getValue().size() ; 
-			if(size== myMap.size()) // if the URL has number of document = number of elements which we search on it 
+			if(size== myMap.size()) // if the URL has number of document = number of elements which we search on it  => doc feha ahmed , w doc feha sabry w doc feha abdelhady
 			{
 				// iterating over all elements and store their positions
 				for(int i = 0 ; i < size ; i ++)
 				{
 					// ana msh mot2kd mn mwdo3 el casting da bs atmna eno yeshtghl
 					Positions.put(newMap.getValue().get(i).getString("Word"), 
-							(ArrayList<Integer>)newMap.getValue().get(i).getList("POSITION",
+							(ArrayList<Integer>)newMap.getValue().get(i).getList("POSITION", // h3dy 3la ahmed ageb el positions bta3tha w a3dy 3la sabry ageb el positions w keda
 									Integer.class));					
 				}
 				
 				Iterator<Entry<String , ArrayList<Integer>>> IT = Positions.entrySet().iterator() ;
-				Map.Entry<String, ArrayList<Integer>> FirstRow = IT.next(); 
+				Map.Entry<String, ArrayList<Integer>> FirstName = IT.next(); 
 				
 				// hena b5ly shoghly dayman 3la awl kelma fl searching  w bdwr 3ala el sequance menha baa
-				for(int j = 0 ; j < size ; j++) {
-					if(compare (size , FirstRow.getValue().get(j) , IT, 2)); // el lvlIndc = 2 34an lw el size = 4 ab2a akny shaghal 1 based fa na habd2 mn level 2
+				for(int m = 0; m < size ; m++) {
+					if(compare (size , FirstName.getValue().get(m) , IT, 2)); // el lvlIndc = 2 34an lw el size = 4 ab2a akny shaghal 1 based fa na habd2 mn level 2
 						{
 							// check for at least 1 lw l2et 
 							// add the URl to the founded
 							URLContainer.add(newMap.getKey());
-							break  ; 
+							break  ; // e5rog w dwr 3la URL tany ykon feh el sequence da
 						}
 				}
 			}
@@ -156,7 +159,7 @@ public class QueryProcessor {
 	public static Boolean compare (int size , Integer number , Iterator<Entry<String , ArrayList<Integer>>> IT, int LvlInd)
 	{
 		// base case 
-		if(LvlInd == size) { // last level 
+		if(LvlInd == size) { // last level 34an ana shghal 1 based
 			Map.Entry<String, ArrayList<Integer>> FirstRow = IT.next(); 
 			for(int i = 0 ; i < FirstRow.getValue().size(); i++)
 			{
@@ -169,6 +172,7 @@ public class QueryProcessor {
 		
 		// found the sequence 
 		Boolean found = false ;
+		
 		Map.Entry<String, ArrayList<Integer>> FirstRow = IT.next(); 
 		for(int i = 0 ; i < FirstRow.getValue().size(); i++)
 		{
